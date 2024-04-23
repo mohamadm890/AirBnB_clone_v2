5c4e33d467fb0cf3fa298499b000bdb4e1805f88
mohamadm890 <94972216+mohamadm890@users.noreply.github.com>
2024-04-23 09:11:41 -0700

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-"""Flask framework"""
+""" Starts a Flask web app """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """ teardown db"""
-    if storage is not None:
-        storage.close()
+def dispose(exception):
+    """ Remove current session """
+    storage.close()
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """ list of state ids"""
-    data = storage.all(State)
-    return render_template('7-states_list.html', total=data.values())
+@app.route('/states_list')
+def states():
+    """ Display list of all the states """
+    states = storage.all(State)
+    states_list = list(states.values())
+    return render_template('7-states_list.html', states=states_list)
 
 
 if __name__ == '__main__':
